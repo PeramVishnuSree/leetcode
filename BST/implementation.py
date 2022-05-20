b7920d734a6299d165a290bb2e2ca46e2a75e786
vishnu <vjp5266@psu.edu>
2022-05-19 22:28:28 -0700

    bst implementation

---
+++ BST/implementation.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return ("Node({})".format(self.value))
+
+    __repr__ = __str__
+
+
+class BinarySearchTree:
+
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert(self.root, value)
+
+    def _insert(self, node, value):
+        if value < node.value:
+            if node.left is None:
+                node.left = Node(value)
+            else:
+                self._insert(node.left, value)
+        else:
+            if node.right is None:
+                node.right = Node(value)
+            else:
+                self._insert(node.right, value)
