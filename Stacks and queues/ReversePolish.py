d158e04d0cbe2598f31d2fbdcf65c87d7bc693d0
vishnu <vjp5266@psu.edu>
2022-06-09 16:10:17 -0700

    stack

---
+++ Stacks and queues/ReversePolish.py
@@ -0,0 +1,22 @@
+# Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+# Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
+# Note that division between two integers should truncate toward zero.
+# It is guaranteed that the given RPN expression is always valid.
+# That means the expression would always evaluate to a result, and there will not be any division by zero operation.
+
+
+
+class Solution:
+    def evalRPN(self, tokens):
+        stack = []
+        ops = ['/', '*', '+', '-']
+        for i in tokens:
+            if i in ops:
+                m = stack.pop()
+                n = stack.pop()
+                string = n + i + m
+                stack.append(str(int(eval(string))))
+            else:
+                stack.append(i)
+
+        return int(stack[0])
